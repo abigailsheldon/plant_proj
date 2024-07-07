d185d765f823db3e73acf797ef6bd3ca98ba804f
Abigail Sheldon <pocketpuppy@Abigails-MacBook-Pro.local>
2024-07-07 18:46:35 -0400

    table formatting

--- astrology_plant.py
+++ astrology_plant.py
@@ -9,14 +9,13 @@ Created on Mon Jul  1 11:18:01 2024
 import json
 import requests
 import sqlite3
-import os
 import openai
 from openai import OpenAI
+import pandas as pd
+from tabulate import tabulate
 
 
-USER_KEY = 'sk-proj-l9zydIhqqCjdi8wrz9MYT3BlbkFJO1rbX8CEx48OmxTOVuBB'
-PROJ_API_KEY = 'sk-proj-B5fkI6Yc6kAIbrn9qvPxT3BlbkFJfPSV3pbaA2wpbbxLNna2'
-
+USER_KEY = 'KEY'
 
 # Create an OpenAPI client
 client = OpenAI(
@@ -24,51 +23,31 @@ client = OpenAI(
     )
 
 
-astro_decriptions = {
-    "aries": ("An Aries was born within March 21-April 20. "
-              "Aries are natural born leaders. They are tenacious and "
-              "like taking risks. They enjoy new experiences and are very "
-              "passionate. They are energetic, creative, caring, and generous."
-              "Aries are fiery, passionatd, determined, and courageous."),
+conn = sqlite3.connect('plants.db')
+cursor = conn.cursor()
 
-    "taurus": ("A Taurus was born within April 21-May 21. "
-               "Taurus is loyal, generous, kind, and patient."),
 
-    "gemini": ("A Gemini was born within May 22-June 21. "
-               "Gemini are independent, curious, charming, and gentle."),
+# Create table to hold data from API
+def create_table():
+    # cursor.execute('DROP TABLE IF EXISTS plant_star_rec')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS plant_star_rec (
+        plant_name TEXT,
+        birthday TEXT,
+        star_sign TEXT
+    )
+    ''')
+    conn.commit()
 
-    "cancer": ("A Cancer was born within June 22-July 23. "
-               "Cancer are compassionate, sentimental, protective, and "
-               "emotional."),
 
-    "leo": ("A Leo was born within July 24-Aug 23. "
-            "Leo are charismatic, dramatic, confident, and fun."),
-
-    "libra": ("A Libra was born within Sept 24-Oct 23. "
-              "Libra are romantic, artistic, friendly, and persuasive."),
-
-    "scorpio": ("A Scorpio was born within Oct 24-Nov 22. "
-                "Scorpio are mysterious, powerful, brave, and resourceful."),
-
-    "aquarius": ("An Aquarius was born within Jan 21-Feb 19. "
-                 "Aquarius are humanitarian, eccentric, individualistic, "
-                 "and cool."),
-
-    "pisces": ("A Pisces was born within Feb 20-March 20. "
-               "Pisces are creative, easy-going, faithful, and sensitive."),
-
-    "capricorn": ("A Capricorn was born within Dec 22-Jan 20. "
-                  "Capricorn are traditional, disciplined, ambitious, "
-                  "and independent."),
-
-    "sagittarius": ("A Sagittarius was born within Nov 23-Dec 21. "
-                    "Sagittarius are philosophical, optimistic, "
-                    "adventurous, and free-spirited."),
-
-    "virgo": ("A Virgo was born within Aug 24-Sept 23. "
-              "Virgo are humble, practical, organized, and "
-              "patient. They are reliable and clever.")
-            }
+def store_plant_rec(plant,bday,star_sign):
+    cursor.execute('''
+    INSERT INTO plant_star_rec (
+        plant_name, birthday, star_sign
+    ) VALUES (?, ?, ?)
+    ''', (plant, bday, star_sign)
+    )
+    conn.commit()
 
 
 def validate_str_input(prompt, options):
@@ -143,8 +122,9 @@ def get_chatgpt_response(prompt):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-16k",
         messages=[
-            {"role": "system", "content": ("You are a houseplant and "
-                                           "astrology expert.")},
+            {"role": "system", "content": (f"You are a houseplant and "
+                                           f"astrology expert. You can describe"
+                                           f"both topics well in few words.")},
             {"role": "user", "content": prompt}
             ]
     )
@@ -157,6 +137,8 @@ def main():
     bday = validate_date_input()
     bday_month = retrieve_bday_month(bday)
     sun_sign = get_astrological_sign(bday[0], bday[1])
+    birthday_formatted = (f"{bday_month} {bday[1]}")
+    create_table()
 
     sunlight_options = [
                 'full sun',
@@ -181,48 +163,52 @@ def main():
 
     # Take sunlight preferences
     sunlight_pref = validate_str_input(
-        ("Preferred sunlight (e.g., 'full sun', 'part shade', "
-         "'full shade','part sun/part shade'): "
+        ("Preferred sunlight "
+         "(e.g., 'full sun', 'part shade', 'full shade','part sun/part shade'): "
         ),
         sunlight_options)
 
     # Take watering freq. preferences
     watering_pref = validate_str_input(
-        "Preferred watering (e.g., 'frequent', 'minimum', 'average'): ",
+        "Preferred watering "
+        "(e.g., 'frequent', 'minimum', 'average'): ",
         watering_options)
 
     # Take maintenance level pref.
     maintenance_pref = validate_str_input(
-        "Preferred maintenance level (e.g., 'low', 'moderate', 'high'): ",
+        "Preferred maintenance level "
+        "(e.g., 'low', 'moderate', 'high'): ",
         maintenance_options)
     
-    print(f"\nYour birthday is {bday_month} {bday[1]}. Your sun sign is {sun_sign.capitalize()}.\n")
+    print(f"\nYour birthday is {bday_month} {bday[1]}. "
+          f"Your sun sign is {sun_sign.capitalize()}.\n")
+    print(f"\nYou prefer a plant that needs {sunlight_pref}, "
+          f"{watering_pref} watering, and is {maintenance_pref} maintenance.")
     
-    prompt = (f"My birthday is {bday_month} {bday[1]} and my star sign is {sun_sign.capitalize()}."
-              f"Name a plant that meets my preferences and astrology sign. Just give me the plant name."
-              f"Say nothing other than the plant name. Do not use punctuation.")
+    
+    prompt1 = (f"Name a houseplant that matches {sun_sign.capitalize()}, "
+              f"is {maintenance_pref} maintenance, and needs {sunlight_pref}, "
+              f"{watering_pref} watering."
+              f"Only reply with the common name and scientific name, "
+              f"formatted like: Common Name (Scientific Name)")
 
-    print(get_chatgpt_response(prompt))
+    plant_rec = get_chatgpt_response(prompt1)
+
+    store_plant_rec(plant_rec,birthday_formatted,sun_sign)
+
+    prompt2 = (f"Please give me brief plant care instructions for {plant_rec} "
+               f"and explain why it matches my star sign.")
+    print(f"\n{get_chatgpt_response(prompt2)}\n")
+
+
+    cursor.execute('SELECT * FROM plant_star_rec')
+    plant_rec_list = cursor.fetchall()
+
+    #df = pd.DataFrame(plant_rec_list, columns = ["Plant", "B-day", "Sign"])
+    #print(df)
+
+    tbl_columns = ["Plant", "B-day", "Sign"]
+    print(tabulate(plant_rec_list, headers = tbl_columns, tablefmt='fancy_grid'))
 
 
 main()
-
-
-'''
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-        
-        {"role": "system", "content": ("You are a houseplant expert and can explain plant care instructions"
-                                       " clearly in a few words. You are also an astrology expert. "
-                                       " You know all the astrological signs very well.")
-         },
-        {"role": "user", "content": (f"My birthday is {bday_month} {bday[1]} and my star sign is {sun_sign.capitalize()}. "
-                                     f" I prefer a plant that needs {sunlight_pref}, {watering_pref} watering, and "
-                                     f" requires {maintenance_pref} maintenance. "
-                                     f"What is the best housplant for me based on my sign and preferences and why?")
-                                     }
-    ]
-)
-print(completion.choices[0].message.content) 
-'''
